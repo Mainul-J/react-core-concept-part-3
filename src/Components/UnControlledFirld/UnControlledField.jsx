a4ec3f407064aad254d3bf0ce4ee19dc98be47db
Mainul-J <mainulh06@gmail.com>
2026-08-12 01:28:59 +0600

    UnControlled Field

--- src/Components/UnControlledFirld/UnControlledField.jsx
+++ src/Components/UnControlledFirld/UnControlledField.jsx
@@ -1,13 +1,30 @@
+import { useRef } from "react";
 
 
 const UnControlledField = () => {
+    const emailRef = useRef('');
+    const passwordlRef = useRef('');
+    const handleSubmit = e =>{
+        e.preventDefault();
+        console.log(emailRef.current.value);
+        const email = emailRef.current.value;
+        const password = passwordlRef.current.value;
+        console.log(email,password);
+    }
+
     return (
         <div>
             <div>
                 <h1>uncontrolled field 46.3 modules 8th mint</h1>
             </div>
             <div>
-                
+                <form onSubmit={handleSubmit}>
+                    <input ref={emailRef} type="email" name="email" placeholder="@email"  />
+                    <br />
+                    <input ref={passwordlRef} type="password" name="password" placeholder="password"  />
+                    <br />
+                    <input type="submit" value="submit" />
+                </form>
             </div>
         </div>
     );
